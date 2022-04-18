9ce0db5e48c9bcc49717eeb91b1d73af76e296ca
Ilya <ilyholu@gmail.com>
2022-04-18 14:42:52 +0300

    Subscribe section

--- src/app.js
+++ src/app.js
@@ -5,3 +5,11 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
